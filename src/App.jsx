5728bb93fd4928e1790fcb5fcae1a990d69db259
Tesfamichael.A <tesfa722@gmail.com>
2023-08-10 11:16:20 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const App = () => {
+  return (
+    <>
+      <div className="bg-black text-4xl text-white flex justify-center items-center p-5 m-4 font-thin">
+        Hello World
+      </div>
+    </>
+  )
+}
+
+export default App
